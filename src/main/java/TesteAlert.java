15a38b1d30b0eacabc98f9ae8ccb916aeb12f428
fabiana.angelo <fabianna.info@gmail.com>
2019-12-22 19:20:07 -0300

    adicionado metodo de alert confirm

--- src/main/java/TesteAlert.java
+++ src/main/java/TesteAlert.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class TesteAlert {
+	
 	@Test
 	public void deveInteragirComAlertSimples() {
 		System.setProperty("webdriver.gecko.driver", "C:\\Users\\WDA Tecnologia\\Documents\\drivers\\geckodriver.exe");
@@ -20,6 +21,29 @@ public class TesteAlert {
 		alert.accept();
 		
 		driver.findElement(By.id("elementosForm:nome")).sendKeys(texto);
+		
+		driver.quit();
+	}
+	
+	@Test
+	public void deveInteragirComAlertConfirm() {
+		System.setProperty("webdriver.gecko.driver", "C:\\Users\\WDA Tecnologia\\Documents\\drivers\\geckodriver.exe");
+		WebDriver driver = new FirefoxDriver(); 
+		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+		
+		driver.findElement(By.id("confirm")).click();
+		Alert alert = driver.switchTo().alert();
+		Assert.assertEquals("Confirm Simples", alert.getText());
+		alert.accept();
+		Assert.assertEquals("Confirmado", alert.getText());
+		alert.accept();
+		
+		driver.findElement(By.id("confirm")).click();
+		alert = driver.switchTo().alert();
+		Assert.assertEquals("Confirm Simples", alert.getText());
+		alert.dismiss();
+		Assert.assertEquals("Negado", alert.getText() );
+		alert.dismiss();
 		
 		driver.quit();
 	}
