8338283db077b1ede861912e66ef391019292e2e
fabiana.angelo <fabianna.info@gmail.com>
2019-12-23 22:02:37 -0300

    adicionado metodo para validacao do campo nome obrigatorio

--- src/main/java/TesteCadastro.java
+++ src/main/java/TesteCadastro.java
@@ -1,5 +1,6 @@
 import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -32,5 +33,17 @@ public class TesteCadastro {
 		Assert.assertTrue(driver.findElement(By.id("descComida")).getText().endsWith("Pizza"));
 		Assert.assertEquals("Escolaridade: superior", driver.findElement(By.id("descEscolaridade")).getText());
 		Assert.assertEquals("Esportes: Natacao Corrida", driver.findElement(By.id("descEsportes")).getText());
+	}
+	
+	@Test
+	public void deveValidarNomeObrigatorio() {
+		System.setProperty("webdriver.gecko.driver", "C:\\Users\\WDA Tecnologia\\Documents\\drivers\\geckodriver.exe");
+		WebDriver driver = new FirefoxDriver(); 
+		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+		
+		driver.findElement(By.id("elementosForm:cadastrar")).click();
+		Alert alert = driver.switchTo().alert();
+		Assert.assertEquals("Nome eh obrigatorio", alert.getText());
+		driver.quit();
 	}
 }
