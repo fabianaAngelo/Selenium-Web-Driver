9dac4f6fc567d97194d182e9c1f27623d939d409
fabiana.angelo <fabianna.info@gmail.com>
2019-12-23 22:06:32 -0300

    adicionado metodo para validacao do campo sexo obrigatorio

--- src/main/java/TesteCadastro.java
+++ src/main/java/TesteCadastro.java
@@ -59,4 +59,18 @@ public class TesteCadastro {
 		Assert.assertEquals("Sobrenome eh obrigatorio", alert.getText());
 		driver.quit();
 	}
+	
+	@Test
+	public void deveValidarSexoObrigatorio() {
+		System.setProperty("webdriver.gecko.driver", "C:\\Users\\WDA Tecnologia\\Documents\\drivers\\geckodriver.exe");
+		WebDriver driver = new FirefoxDriver(); 
+		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+		
+		driver.findElement(By.id("elementosForm:nome")).sendKeys("Fabiana");
+		driver.findElement(By.id("elementosForm:sobrenome")).sendKeys("Angelo");
+		driver.findElement(By.id("elementosForm:cadastrar")).click();
+		Alert alert = driver.switchTo().alert();
+		Assert.assertEquals("Sexo eh obrigatorio", alert.getText());
+		driver.quit();
+	}
 }
